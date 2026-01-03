8722d7e72016861f41a6e92ffe7c91d26661714d
Rafael Aguiar <rafaelpaguiar@gmail.com>
2026-01-03 09:35:44 -0300

    Adjusted folders.

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 )
+
+type Words struct {
+	Page  string   `json:"page"`
+	Input string   `json:"input"`
+	Words []string `json:"words"`
+}
 
 func main() {
 	args := os.Args
@@ -31,9 +39,23 @@ func main() {
 	defer response.Body.Close()
 
 	body, err := io.ReadAll(response.Body)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("HTTP Status Code: %d\nBody: %s\n", response.StatusCode, body)
+	if response.StatusCode != 200 {
+		fmt.Printf("Invalid output (HTTP Status Code): %d\nBody: %s\n", response.StatusCode, body)
+		os.Exit(1)
+	}
+
+	var words Words
+
+	err = json.Unmarshal(body, &words)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("JSON Parsed\nPage: %s\nWords: %v", words.Page, strings.Join(words.Words, ", "))
 }
