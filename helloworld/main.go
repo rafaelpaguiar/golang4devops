e1bdddbc5f0fa58fa75a8e243f6f76720518c2be
Rafael Aguiar <rafaelpaguiar@gmail.com>
2026-01-05 09:09:08 -0300

    Added partial json umarshaling.

--- helloworld/main.go
+++ helloworld/main.go
@@ -11,10 +11,18 @@ import (
 	"strings"
 )
 
+type Page struct {
+	Name string `json:"page"`
+}
+
+
 type Words struct {
-	Page  string   `json:"page"`
 	Input string   `json:"input"`
 	Words []string `json:"words"`
+}
+
+type Occurrence struct {
+	Words map[string]int `json:"words"`	
 }
 
 func main() {
@@ -33,29 +41,61 @@ func main() {
 	response, err := http.Get(args[1])
 
 	if err != nil {
-		log.Fatal(err)
+			log.Fatal(err)
+		}
+
+		defer response.Body.Close()
+
+		body, err := io.ReadAll(response.Body)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if response.StatusCode != 200 {
+			fmt.Printf("Invalid output (HTTP Status Code): %d\nBody: %s\n", response.StatusCode, body)
+			os.Exit(1)
+		}
+
+		var page Page
+
+		err = json.Unmarshal(body, &page)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		switch(page.Name){
+		case "words":
+			var words Words
+
+			err = json.Unmarshal(body, &words)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Printf("JSON Parsed\nPage: %s\nWords: %v\n", page.Name, strings.Join(words.Words, ", "))
+
+		case "occurrence":
+
+			var occurrence Occurrence
+
+			err = json.Unmarshal(body, &occurrence)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if val, ok := occurrence.Words["word1"]; ok {
+				fmt.Printf("Found word1: %d\n", val)
+			}
+
+			for word, occurrence := range occurrence.Words {
+				fmt.Printf("%s: %d\n", word, occurrence)
+			}
+
+		default:
+			fmt.Printf("Page not found.\n")
 	}
 
-	defer response.Body.Close()
-
-	body, err := io.ReadAll(response.Body)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if response.StatusCode != 200 {
-		fmt.Printf("Invalid output (HTTP Status Code): %d\nBody: %s\n", response.StatusCode, body)
-		os.Exit(1)
-	}
-
-	var words Words
-
-	err = json.Unmarshal(body, &words)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("JSON Parsed\nPage: %s\nWords: %v", words.Page, strings.Join(words.Words, ", "))
 }
